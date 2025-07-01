2d5f9e42a43ef63defcc27bd5a8e1db523d4a08d
bunnybunnyy <hanifumar169@gmail.com>
2025-07-01 23:18:03 +0500

    updatedgfgdgfc

--- src/components/socket.js
+++ src/components/socket.js
@@ -7,6 +7,6 @@ export const initSocket = async () => {
         timeout: 10000,
         transports: ['websocket', 'polling']
     };
-    const serverUrl = process.env.REACT_APP_BACKEND_URL || 'https://quick-code-ol6a.onrender.com';
+    const serverUrl = process.env.REACT_APP_BACKEND_URL || 'https://localhost:5000';
     return io(serverUrl, options);
 };
